54810246c6019a0462f0c4c1ed6b3887abbbae3a
Fernando J Pando <nando@hex7.com>
2016-10-29 10:28:47 -0400

    nixos-configurations

---
+++ toshiba-satellite-p740d.nix
@@ -0,0 +1,136 @@
+{ config, pkgs, ... }:
+{
+  #
+  # This NixOS configuration is for the
+  #   ==> Toshiba Satellite P740D <==
+  #
+
+  imports = [ ./hardware-configuration.nix ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+  boot.extraModprobeConfig = "options snd_hda_intel enable=0,1";
+  #boot.blacklistedKernelModules = [ "snd_pcsp" ];
+
+  hardware.trackpoint = {
+    enable = true;
+    sensitivity = 50;
+    speed = 50;
+    fakeButtons = true;
+  };
+
+  hardware.pulseaudio.enable = true;
+
+  networking.hostName = "abraxas";
+  networking.wireless.enable = true;
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 22 ];
+
+  security.sudo.enable = true;
+  security.sudo.wheelNeedsPassword = false;
+
+  time.timeZone = "America/New_York";
+  i18n = {
+     consoleFont = "Lat2-Terminus16";
+     consoleKeyMap = "us";
+     defaultLocale = "en_US.UTF-8";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  #nixpkgs.config.allowBroken = true;
+
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    vimPlugins.vim-nix
+    vimPlugins.vim-go
+    vimPlugins.vim-jsonnet
+    vimPlugins.vim-jinja
+    firefox
+    lsof
+    pciutils
+    tcpdump
+    netcat
+    jwhois
+    strace
+    google-chrome
+    spotify
+    openvpn
+    gimp
+    docker
+    terraform
+    ansible2
+    chefdk
+    vagrant
+    virtualbox
+    packer
+    jenkins
+    git
+    vlc
+    mplayer
+    ruby
+    python27Full
+    python27Packages.virtualenv
+    awscli
+    nginx
+    htop
+    gnupg
+    parted
+    nmap
+    imagemagick
+    screen
+    xpdf
+    unzip
+
+    kde4.kdeadmin
+    kde4.kdeartwork
+    kde4.kdebindings
+    kde4.kdegraphics
+    kde4.kdelibs
+    kde4.kdemultimedia
+    kde4.kdenetwork
+    kde4.kdepim
+    kde4.kdesdk
+    kde4.kdeutils
+
+    #gerrit
+    #xmms
+  ];
+
+  services.openssh.enable = true;
+  services.printing.enable = true;
+  services.locate.enable = true;
+  services.ntp.enable = true;
+
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.displayManager.kdm.enable = true;
+  services.xserver.desktopManager.kde4.enable = true;
+  #services.xserver.windowManager.openbox.enable = true;
+
+  virtualisation.docker.enable = true;
+  virtualisation.docker.storageDriver = "btrfs";
+
+  users.extraUsers.lori = {
+    isNormalUser = true;
+    createHome = true;
+    home = "/home/lori";
+    uid = 1000;
+  };
+
+  users.extraUsers.nando = {
+    isNormalUser = true;
+    createHome = true;
+    home = "/home/nando";
+    extraGroups = [ "wheel" ];
+    uid = 1001;
+  };
+
+  users.extraUsers.nando.openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwH1rWuQJXZXXgyWmJp6ripDLSyTGteNkvsn4AO/Bqo+TWSX1bxmDH4uk94D2/YOsRQiPs+dDHuJuBIqZnZicnOhbQFzi4EegV1S9Xw4ZWzJu9JT6dcI3ThOlQ2LVeEYajo+A1eoTdr5Hkfs79w+9FvLjYHgbuhvcsR5n9jFHynM0JPjcnDR7wNnDdqFoQqUFHG6nyJ3MotUBQGWuH/iDGOxcefHCbazdYTj4nFtbVtkAX8qRDz0ajlXGIhCVnV5/K7U1ZpXOlRIc8Ylt/v3DQsyvedUIyPrGLvzYx1tJXTbPWK3gXHAYRvDsydrCGfwiVCPK29Vfewy8fBaO/tdJB" ];
+
+  users.extraUsers.root.openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwH1rWuQJXZXXgyWmJp6ripDLSyTGteNkvsn4AO/Bqo+TWSX1bxmDH4uk94D2/YOsRQiPs+dDHuJuBIqZnZicnOhbQFzi4EegV1S9Xw4ZWzJu9JT6dcI3ThOlQ2LVeEYajo+A1eoTdr5Hkfs79w+9FvLjYHgbuhvcsR5n9jFHynM0JPjcnDR7wNnDdqFoQqUFHG6nyJ3MotUBQGWuH/iDGOxcefHCbazdYTj4nFtbVtkAX8qRDz0ajlXGIhCVnV5/K7U1ZpXOlRIc8Ylt/v3DQsyvedUIyPrGLvzYx1tJXTbPWK3gXHAYRvDsydrCGfwiVCPK29Vfewy8fBaO/tdJB" ];
+
+  system.stateVersion = "16.09";
+
+}
